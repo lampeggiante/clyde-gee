2dc20b4c309652681a96d88d7bf5cf5c248b6fb9
clyde <1099052496@qq.com>
2026-04-26 16:16:21 +0800

    feat: 抽离context和router

--- gee/gee.go
+++ gee/gee.go
@@ -1,24 +1,22 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 // Engine 创建工厂方法
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 // GET
@@ -38,10 +36,6 @@ func (engine *Engine) Run(addr string) (err error) {
 
 // ServeHTTP
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL.Path)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
